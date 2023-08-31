5d6268d51c5154798ae36d8dc5af23382c5c26ed
Eduardo Assis <84932877+eduardohenriqueassis@users.noreply.github.com>
2023-08-30 23:19:15 -0300

    set deploy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/medication_time_table_react",
+});
